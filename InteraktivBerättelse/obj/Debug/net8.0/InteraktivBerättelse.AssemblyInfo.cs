1bd59226ef84095d6399bbae1abeb4f9bbbba926
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-09-06 10:40:28 +0200

    htehte

--- InteraktivBerättelse/obj/Debug/net8.0/InteraktivBerättelse.AssemblyInfo.cs
+++ InteraktivBerättelse/obj/Debug/net8.0/InteraktivBerättelse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InteraktivBerättelse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff910ebaa626c7151c94e63769c5801045c3514e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207ddbad6f1146915175521676b96e5c4061bdb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("InteraktivBerättelse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InteraktivBerättelse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
